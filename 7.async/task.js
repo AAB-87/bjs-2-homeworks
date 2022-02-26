c1489ed730ef014c94b6b387855e9bfb820e4d7c
Andrey <andrej-abrekov@yandex.ru>
2022-02-26 17:21:07 +0400

    Fixed method Start

--- 7.async/task.js
+++ 7.async/task.js
@@ -19,7 +19,7 @@ class AlarmClock { // создаём будильник
 		let firstArr = this.alarmCollection.length; // узнаём длину существующего массива и сохраняем её в переменную
 		this.alarmCollection = this.alarmCollection.filter((element) => element.id 	!== id); // фильтруем массив и сохраняем его
 		let secondArr = this.alarmCollection.length; // сохраняем длину фильтрованного массива в новую переменную
-		return firstArr > secondArr; // сравниваем переменнные
+		return firstArr > secondArr; // сравниваем переменные
 	}
 
  	getCurrentFormattedTime() {
@@ -27,19 +27,20 @@ class AlarmClock { // создаём будильник
 	}
 
 	start() {
-		function checkClock(id) { // функция проверки
-			if (addClock.time == getCurrentFormattedTime) { // если текущее время совпадает со временем звонка, то вызываеся колбек
-				return callback;
-			}
-		}
 		if (this.timerId === null) { // если значение идентификатора текущего таймера отсутствует, то создайте новый интервал
-			this.timerId = setInterval(() => {this.alarmCollection.forEach(id => checkClock(id)); }, 1000); // в интервале реализована функция, которая будет перебирать все звонки, и для каждого вызывать функцию checkClock
+			this.timerId = setInterval(() => {this.alarmCollection.forEach(id => this.checkClock(id)); }, 1000); // в интервале реализована функция, которая будет перебирать все звонки, и для каждого вызывать функцию checkClock
+		}
+	}
+
+	checkClock(clock) { // функция проверки
+		if (clock.time == this.getCurrentFormattedTime()) { // если текущее время совпадает со временем звонка, то вызывается колбек
+			return clock.callback();
 		}
 	}
 
 	stop() { // метод останавливает выполнение всех звонков
 		if (this.timerId !== null) { // проверка существования идентификатора текущего таймера
-			clearInterval(this.timerId); // если у идентификатора текущего таймера есть значение, то вызоваем функцию clearInterval для удаления интервала
+			clearInterval(this.timerId); // если у идентификатора текущего таймера есть значение, то вызываем функцию clearInterval для удаления интервала
 		}
 		return this.timerId = null; // а так же удаляем значение из свойства "идентификатор текущего таймера"
 	}
